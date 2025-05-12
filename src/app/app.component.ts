0328d02506d241111ec5216d2592c152a0e3f878
Lara <lara.salman01@hotmail.com>
2025-05-12 15:08:36 +0200

    create navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet, LayoutComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './layout.component.html',
   standalone: true,
 })
