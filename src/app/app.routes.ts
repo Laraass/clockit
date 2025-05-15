67bf7504bf5694ec056d5a210870d9e2e5c225d6
Lara <lara.salman01@hotmail.com>
2025-05-15 21:33:22 +0200

    set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { TimeReportComponent } from './components/time-report/time-report.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'register', pathMatch: 'full' }, // Change later
+      { path: 'register', component: RegisterComponent },
+      { path: 'time-report', component: TimeReportComponent }
+    ]
+  }
+];
