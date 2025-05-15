2340f559321e191bf4efcd9bbd56300312ce506f
Lara <lara.salman01@hotmail.com>
2025-05-15 21:50:09 +0200

    set login as start page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      { path: '', redirectTo: 'register', pathMatch: 'full' }, // Change later
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'register', component: RegisterComponent },
       { path: 'login', component: LoginComponent },
     ]
