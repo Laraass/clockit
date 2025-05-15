00a05ed2a7934cc9f2cb67333ab70b47851c985f
Lara <lara.salman01@hotmail.com>
2025-05-15 22:15:19 +0200

    create log in page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'app/services/auth.service';
+import { Router } from '@angular/router';
+import { ButtonComponent } from 'app/components/button/button.component';
+import { InputfieldComponent } from 'app/components/inputfield/inputfield.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { LoginCredentials, LoginResponse } from 'app/services/auth.types';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  standalone: true,
+  imports: [InputfieldComponent, ButtonComponent, CommonModule, FormsModule],
 })
 export class LoginComponent {
+  user: LoginCredentials = {
+    email: '',
+    password: '',
+  };
 
+  errorMessage: string | null = null;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(): void {
+    this.errorMessage = null;
+
+    if (!this.user.email || !this.user.password) {
+      this.errorMessage = 'Please enter both email and password.';
+      return;
+    }
+
+    this.authService.login(this.user).subscribe({
+      next: (res: LoginResponse) => {
+        console.log('Login successful:', res);
+
+        if (res.token) {
+          this.authService.saveToken(res.token);
+        }
+
+        this.router.navigate(['/']); // add route to home page
+      },
+      error: (err) => {
+        console.error('Login failed:', err);
+        this.errorMessage =
+          err.error?.message || 'Login failed. Please try again.';
+      },
+    });
+  }
 }
